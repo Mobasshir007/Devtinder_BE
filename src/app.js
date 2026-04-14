c06bcc99f93546e25cb20773e8dfcd754d624128
Mobasshir <mobasshirhussain2002@gmail.com>
2026-04-15 00:23:53 +0530

    ...

--- src/app.js
+++ src/app.js
@@ -8,8 +8,16 @@ app.listen(777,()=>{
 app.use("/test/2",(req,res)=>{
     res.send("This is test 2")
 })
-app.use("/test",(req,res)=>{
+app.get("/test",(req,res,next)=>{
+    console.log("1st route");
+    
     res.send("Hello world")
+    next()
+},
+(req,res,next)=>{
+    console.log("2nd route");
+    
+    res.send("2nd route handler")
 })
 app.use("/",(req,res)=>{
     res.send("Home page")
