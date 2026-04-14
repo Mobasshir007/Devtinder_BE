b61f05c75b4a9a95dc7a7a1cc28b6e9012933792
Mobasshir <mobasshirhussain2002@gmail.com>
2026-04-14 23:12:04 +0530

    practice

--- src/app.js
+++ src/app.js
@@ -5,7 +5,12 @@ app.listen(777,()=>{
     console.log("Server is running on port 777");
     
 })
-
-app.use((req,res)=>{
-    res.send("Hello worldasdhjk")
+app.use("/test/2",(req,res)=>{
+    res.send("This is test 2")
+})
+app.use("/test",(req,res)=>{
+    res.send("Hello world")
+})
+app.use("/",(req,res)=>{
+    res.send("Home page")
 })
